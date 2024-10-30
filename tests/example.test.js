2bdb20d6674b9698b9f7d34a1ff235076543581f
Mourya-K <98205221+Mourya-K@users.noreply.github.com>
2024-10-30 18:44:23 +0530

    Update example.test.js

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,7 +4,7 @@ const { Builder, By, until } = require('selenium-webdriver');
     let driver = await new Builder().forBrowser('chrome').build();
     try {
         await driver.get('http://localhost:2020');
-        await driver.wait(until.titleIs('Hello, Docker with Node.js!'), 10000); // wait for 10 seconds
+        await driver.wait(until.titleIs('Hello, Docker with Node.js!'), 30000); // wait for 30 seconds
         let title = await driver.getTitle();
         console.log(title); // Should print "Hello, Docker with Node.js!"
     } finally {

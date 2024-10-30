ae4e504e3f6075ca2e9c1806e11e0f1696b6b4ff
Mourya-K <98205221+Mourya-K@users.noreply.github.com>
2024-10-30 17:03:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello, Docker with Node.js!');
 });
 
-const port = 80;
+const port = 2020;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
